c2983274e8ec404b63b28eb0e98f18ac4af832a2
Steven Cai <szcai@ad.unc.edu>
2025-04-28 12:14:59 -0400

    add text on work slides

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let sliderIndex = 0;
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slider img");
-const dots = document.querySelectorAll(".slider-nav a");
-const slidesText = document.querySelectorAll(".slide-text");
+// let sliderIndex = 0;
+// let currentSlide = 0;
+// const slides = document.querySelectorAll(".slider img");
+// const dots = document.querySelectorAll(".slider-nav a");
+// const slidesText = document.querySelectorAll(".slide-text");
 
-function plusSlides(n) {
-  currentSlide += n;
+// function plusSlides(n) {
+//   currentSlide += n;
 
-  // Pressing prev on the first image should loop to the last image
-  if (currentSlide < 0) currentSlide = slides.length - 1;
+//   // Pressing prev on the first image should loop to the last image
+//   if (currentSlide < 0) currentSlide = slides.length - 1;
 
-  // Pressing next on the last image should loop to the first image
-  if (currentSlide >= slides.length) currentSlide = 0;
+//   // Pressing next on the last image should loop to the first image
+//   if (currentSlide >= slides.length) currentSlide = 0;
 
-  // Fade in and out animation
-  slides.forEach((slide) => (slide.style.display = "none"));
-  slides[currentSlide].style.display = "block";
+//   // Fade in and out animation
+//   slides.forEach((slide) => (slide.style.display = "none"));
+//   slides[currentSlide].style.display = "block";
 
-  // Remove Active dots
-  dots.forEach((dot) => dot.classList.remove("active-dot"));
-  // Add to current dot
-  dots[currentSlide].classList.add("active-dot");
+//   // Remove Active dots
+//   dots.forEach((dot) => dot.classList.remove("active-dot"));
+//   // Add to current dot
+//   dots[currentSlide].classList.add("active-dot");
 
-  updateTextVisibility();
-}
+//   updateTextVisibility();
+// }
 
-function updateTextVisibility() {
-  slidesText.forEach((slideText, index) => {
-    const isActive = index === currentSlide;
-    slideText.style.opacity = isActive ? "1" : "0";
-    slideText.style.pointerEvents = isActive ? "auto" : "none";
-  });
-}
+// function updateTextVisibility() {
+//   slidesText.forEach((slideText, index) => {
+//     const isActive = index === currentSlide;
+//     slideText.style.opacity = isActive ? "1" : "0";
+//     slideText.style.pointerEvents = isActive ? "auto" : "none";
+//   });
+// }
 
 document.querySelectorAll(".slider-wrapper").forEach((wrapper) => {
   let currentSlide = 0;
